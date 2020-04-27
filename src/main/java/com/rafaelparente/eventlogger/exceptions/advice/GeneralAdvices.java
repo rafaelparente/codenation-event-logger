19cd9cb586d46437948a334937d9a6cc8fc766cf
Rafael Parente <rafaelp.dev@gmail.com>
2020-04-26 23:18:53 -0300

    [MOD] Annotations for advices.

--- src/main/java/com/rafaelparente/eventlogger/exceptions/advice/GeneralAdvices.java
+++ src/main/java/com/rafaelparente/eventlogger/exceptions/advice/GeneralAdvices.java
@@ -4,18 +4,14 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Order(Ordered.LOWEST_PRECEDENCE)
 public class GeneralAdvices {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
     public String handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
 
         return ex.getBindingResult().getFieldError().getField() + " - " + ex.getBindingResult().getFieldError().getDefaultMessage();
@@ -23,7 +19,6 @@ public class GeneralAdvices {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
     public String handleException(Exception ex) {
         return "Internal Server Error: " + ex.getMessage();
     }
--- src/main/java/com/rafaelparente/eventlogger/exceptions/advice/UserAlreadyExistsAdvice.java
+++ src/main/java/com/rafaelparente/eventlogger/exceptions/advice/UserAlreadyExistsAdvice.java
@@ -4,16 +4,12 @@ import com.rafaelparente.eventlogger.exceptions.UserAlreadyExistsException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class UserAlreadyExistsAdvice {
 
-    @ResponseBody
     @ExceptionHandler(UserAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.OK)
     public String userAlreadyExistsHandler(UserAlreadyExistsException ex) {
